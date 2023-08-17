4008b3dbe8814c7a7073c4efc26e6e9864f15198
Mouad Lahlal <76909759+MouadLahlal@users.noreply.github.com>
2023-08-17 21:43:05 +0200

    Update settings.js

--- src/api/settings.js
+++ src/api/settings.js
@@ -1,7 +1,7 @@
 export const apiData = {
     // apiHost: "https://todo-api-5xyp.onrender.com",
-    // apiHost: "https://odd-rose-kitten-kilt.cyclic.app/",
-    apiHost: "http://localhost:3001",
+    apiHost: "https://odd-rose-kitten-kilt.cyclic.app/",
+    // apiHost: "http://localhost:3001",
     // apiHost: "http://192.168.143.101:3001",
     apiPort: ""
 }
